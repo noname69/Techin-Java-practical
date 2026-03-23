56dbf8b7fb0c37dc3011b78b98894be2a0daf658
Pavel <Pavel.Stefanovic@gmail.com>
2026-03-23 17:19:21 +0200

    Add 08_3_12.CargoHold excercise solution

--- 08_praktiniai_oop_3/08_3_12.CargoHold/src/main/java/Hold.java
+++ 08_praktiniai_oop_3/08_3_12.CargoHold/src/main/java/Hold.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class Hold {
+    private int maxWeight;
+    private ArrayList<Suitcase> suitcases;
+
+    public Hold(int maxWeight) {
+        this.maxWeight = maxWeight;
+        this.suitcases = new ArrayList<>();
+    }
+
+    public void addSuitcase(Suitcase suitcase) {
+        if(maxWeight >= suitcase.totalWeight()) {
+            maxWeight -= suitcase.totalWeight();
+            suitcases.add(suitcase);
+        }
+    }
+
+    public void printItems() {
+        for(Suitcase suitcase: suitcases) {
+            suitcase.printItems();
+        }
+    }
+
+    @Override
+    public String toString() {
+        int totalWeight = 0;
+        for(Suitcase suitcase: suitcases) {
+            totalWeight += suitcase.totalWeight();
+        }
+        return suitcases.size() + " suitcases ("+ totalWeight +" kg)";
+    }
 }
--- 08_praktiniai_oop_3/08_3_12.CargoHold/src/main/java/Suitcase.java
+++ 08_praktiniai_oop_3/08_3_12.CargoHold/src/main/java/Suitcase.java
@@ -6,22 +6,60 @@ public class Suitcase {
 
     public Suitcase(int maxWeight) {
         this.maxWeight = maxWeight;
+        this.items = new ArrayList<>();
     }
 
     public void addItem(Item item) {
-        if (maxWeight > item.getWeight()) {
+        if (maxWeight >= item.getWeight()) {
             maxWeight -= item.getWeight();
             items.add(item);
         }
 
     }
 
-    @Override
-    public String toString() {
-        int totalWeight = 0;
+    public void printItems() {
+        StringBuilder result = new StringBuilder();
         for(Item item: items) {
+//            result.append(item.getName()).append(" (").append(item.getWeight()).append(")");
+            System.out.println(item.getName() + "(" + item.getWeight() + " kg)");
+        }
+//        result.append("Total Weight: ").append(totalWeight());
+//        System.out.println(result);
+        System.out.println("Total Weight: " + totalWeight() + " kg");
+    }
+
+    public int totalWeight() {
+        int totalWeight = 0;
+        for (Item item : items) {
             totalWeight += item.getWeight();
         }
-        return items.size() + " items (" + totalWeight + " kg)";
+        return totalWeight;
+    }
+
+    public Item heaviestItem() {
+        if(items.isEmpty()) return null;
+
+        Item heaviestItem = items.get(0);
+        for(Item item: items) {
+            if(item.getWeight() > heaviestItem.getWeight()) {
+                heaviestItem = item;
+            }
+        }
+        return heaviestItem;
+    }
+
+    @Override
+    public String toString() {
+        String itemName = "items";
+        if (items.isEmpty()) {
+            itemName = "no items (0 kg)";
+        } else if (items.size() == 1) {
+            itemName = "item";
+        }
+
+
+
+
+        return items.isEmpty() ? itemName : items.size() + " " + itemName + " (" + totalWeight() + " kg)";
     }
 }
